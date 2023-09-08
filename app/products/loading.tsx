20c267d6ee0b8be25ef64d9e82d503b453a5f6c9
yousuf123456 <muhammadyousuf554456@gmail.com>
2023-09-08 21:52:50 +0500

    Exp Change

--- app/products/loading.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { InformationLoading } from "./[productId]/components/containers/loadings/InformationLoading";
-
-export default function loading() {
-  return <InformationLoading />;
-}
