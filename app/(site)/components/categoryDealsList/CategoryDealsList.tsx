7151598c7052e6eab1b438823c09b074b4570252
yousuf123456 <muhammadyousuf554456@gmail.com>
2023-09-08 21:44:00 +0500

    Exp Change

--- app/(site)/components/categoryDealsList/CategoryDealsList.tsx
+++ app/(site)/components/categoryDealsList/CategoryDealsList.tsx
@@ -1,39 +1,35 @@
-import React from 'react'
-import { DealsList } from './components/DealsList';
-import { Button } from '@/app/components/Button';
-import { Heading } from '../Heading';
-import { CtaLink } from '../CtaLink';
-import { getDiscountedProducts } from '@/app/actions/getDiscountedProducts';
+import React from "react";
+import { DealsList } from "./components/DealsList";
+import { Button } from "@/app/components/Button";
+import { Heading } from "../Heading";
+import { CtaLink } from "../CtaLink";
+import { getDiscountedProducts } from "@/app/actions/getDiscountedProducts";
 
-export const CategoryDealsList = async() => { 
-    const category = "Furniture";
-    const discountedProducts = await getDiscountedProducts(true); 
+export const CategoryDealsList = async () => {
+  const category = "Furniture";
+  const discountedProducts = await getDiscountedProducts(true);
 
   return (
-    <div className='flex flex-col gap-2'>
-        <div className='flex justify-between items-center'>
-            <div className=' h-full flex items-end'>
-                <Heading>
-                    Deals On {category}
-                </Heading>
-            </div>
-
-            <div className='hidden sm:block'>
-              <Button variant='default'>
-                  Shop More
-              </Button>
-            </div>
-
-            <div className='block sm:hidden'>
-              <CtaLink href=''>
-                <p className='text-base font-text font-semibold underline text-themeBlue'>
-                  Shop More
-                </p>
-              </CtaLink>
-            </div>
+    <div className="flex flex-col gap-2">
+      <div className="flex items-center justify-between">
+        <div className=" flex h-full items-end">
+          <Heading>Deals On {category}</Heading>
         </div>
 
-        <DealsList products={discountedProducts} />
+        <div className="hidden sm:block">
+          <Button variant="default">Shop More</Button>
+        </div>
+
+        <div className="block sm:hidden">
+          <CtaLink href="">
+            <p className="font-text text-sm font-semibold text-themeBlue underline">
+              Shop More
+            </p>
+          </CtaLink>
+        </div>
+      </div>
+
+      <DealsList products={discountedProducts} />
     </div>
-  )
-}
+  );
+};
