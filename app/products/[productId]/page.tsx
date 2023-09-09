a9e9edbf2fbc1c77d478fb9ee751b75d1e2b24e6
yousuf123456 <muhammadyousuf554456@gmail.com>
2023-09-09 15:33:52 +0500

    Suspense Removed

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -48,53 +48,53 @@ export default async function ProductDetailsPage({
 }: {
   params: IParams;
 }) {
-  await new Promise((resolve) => setTimeout(resolve, 100));
+  // await new Promise((resolve) => setTimeout(resolve, 100));
 
   return (
     <div className="w-full overflow-x-hidden bg-slate-100 sm:px-4 sm:py-4 lg:mt-8 lg:px-8 lg:py-8 xl:px-20">
       <NavigationPanel showSearchBar showCart showShare />
 
       <div className="flex flex-col gap-6">
-        <Suspense fallback={<InformationLoading />}>
-          <Information productId={params.productId} />
-        </Suspense>
+        {/* <Suspense fallback={<InformationLoading />}>
+        </Suspense> */}
+        <Information productId={params.productId} />
 
         <div className="flex items-start gap-6 max-lg:flex-col">
           <div className="flex w-full flex-col gap-5">
             <Container>
-              <Suspense key="details" fallback={<DetailsLoading />}>
-                <Details productId={params.productId} />
-              </Suspense>
+              {/* <Suspense key="details" fallback={<DetailsLoading />}>
+              </Suspense> */}
+              <Details productId={params.productId} />
             </Container>
 
             <Container id="ratings">
-              <Suspense key="reviews" fallback={<ReviewsLoading />}>
-                <Reviews productId={params.productId} />
-              </Suspense>
+              {/* <Suspense key="reviews" fallback={<ReviewsLoading />}>
+              </Suspense> */}
+              <Reviews productId={params.productId} />
             </Container>
 
             <Container id="questions">
-              <Suspense key="questions" fallback={<QuestionsLoading />}>
-                <Questions productId={params.productId} />
-              </Suspense>
+              {/* <Suspense key="questions" fallback={<QuestionsLoading />}>
+              </Suspense> */}
+              <Questions productId={params.productId} />
             </Container>
 
-            <Suspense
+            {/* <Suspense
               key="similarProducts"
               fallback={<SimilarProductsLoading />}
             >
-              <Container>
-                <SimilarProducts productId={params.productId} />
-              </Container>
-            </Suspense>
+            </Suspense> */}
+            <Container>
+              <SimilarProducts productId={params.productId} />
+            </Container>
           </div>
 
-          <Suspense
+          {/* <Suspense
             key="productsfromsamestore"
             fallback={<ProductsFromStoreLoading />}
           >
-            <ProductsFromStore productId={params.productId} />
-          </Suspense>
+          </Suspense> */}
+          <ProductsFromStore productId={params.productId} />
         </div>
       </div>
     </div>
