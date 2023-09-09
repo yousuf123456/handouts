f93ffdf27f4d02b177b0d1670f42689f5585aceb
yousuf123456 <muhammadyousuf554456@gmail.com>
2023-09-09 14:14:10 +0500

    New Change

--- app/products/[productId]/components/containers/loadings/ProductsFromStoreLoading.tsx
+++ app/products/[productId]/components/containers/loadings/ProductsFromStoreLoading.tsx
@@ -1,24 +1,23 @@
 "use client";
 import { Heading } from "@/app/(site)/components/Heading";
 import CircularProgress from "@mui/material/CircularProgress";
+import LinearProgress from "@mui/material/LinearProgress";
 import React from "react";
 import { Container } from "../../Container";
-import { LinearProgress, useMediaQuery } from "@mui/material";
 
 export const ProductsFromStoreLoading = () => {
-  const isLargeDevices = useMediaQuery("(max-width:1024px)");
-
   return (
     <div className="h-full flex-shrink-0 max-lg:w-full">
-      <Container wFit={!isLargeDevices}>
+      <Container className="w-full lg:w-fit ">
         <div className="flex flex-col gap-4 lg:items-center">
           <Heading>From the Same Store</Heading>
 
-          {isLargeDevices ? (
+          <div className="max-lg:w-full lg:hidden">
             <LinearProgress color="secondary" />
-          ) : (
+          </div>
+          <div className="hidden lg:block">
             <CircularProgress color="secondary" />
-          )}
+          </div>
         </div>
       </Container>
     </div>
