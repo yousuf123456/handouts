98b9255b343f9c1c23871f2c35ad375041e4cea5
yousuf123456 <muhammadyousuf554456@gmail.com>
2023-09-27 19:24:05 +0500

    Minor Change

--- app/(site)/components/RecomendedProducts/RecomendedProductsList.tsx
+++ app/(site)/components/RecomendedProducts/RecomendedProductsList.tsx
@@ -1,30 +1,26 @@
-import { getRecomendedProducts } from '@/app/actions/recomendations/getRecomendedProducts'
-import { ProductCard } from '@/app/components/ProductCard';
-import { ProductsListLayout } from '@/app/components/ProductsListLayout';
-import { ProductCardType, fullCategoryDiscountedProductType } from '@/app/types';
-import Image from 'next/image';
-import React from 'react'
+import { getRecomendedProducts } from "@/app/actions/recomendations/getRecomendedProducts";
+import { ProductCard } from "@/app/components/ProductCard";
+import { ProductsListLayout } from "@/app/components/ProductsListLayout";
+import {
+  ProductCardType,
+  fullCategoryDiscountedProductType,
+} from "@/app/types";
+import Image from "next/image";
+import React from "react";
 
-export const RecomendedProductsList = async() => {
-    const recomendedProducts = await getRecomendedProducts() as ProductCardType[];
+export const RecomendedProductsList = async () => {
+  const recomendedProducts =
+    (await getRecomendedProducts()) as unknown as ProductCardType[];
 
-    if(!recomendedProducts){
-        return (
-            <p>Browse some products to start getting recomendations</p>
-        )
-    }
+  if (!recomendedProducts) {
+    return <p>Browse some products to start getting recomendations</p>;
+  }
 
   return (
     <ProductsListLayout>
-        {
-            recomendedProducts.map((product, i)=> (
-                <ProductCard
-                    key={i}
-                    dynamic={true}
-                    product={product}
-                />
-            ))
-        }
+      {recomendedProducts.map((product, i) => (
+        <ProductCard key={i} dynamic={true} product={product} />
+      ))}
     </ProductsListLayout>
-  )
-}
+  );
+};
