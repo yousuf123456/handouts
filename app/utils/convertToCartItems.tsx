c96ade80ca2140526d86a09fe9a320ee07f5fdce
yousuf123456 <muhammadyousuf554456@gmail.com>
2023-09-27 19:30:16 +0500

    Minor Changes

--- app/utils/convertToCartItems.tsx
+++ app/utils/convertToCartItems.tsx
@@ -1,26 +1,30 @@
 import { CartItemType, OrderedProductType } from "../types";
 // outdated function
-export const convertToCartItems = (orderedProducts: OrderedProductType[]): CartItemType[] => {
-    const convertedCartItems = orderedProducts.map((orderedProduct)=> {
-        const convertedCartItem = {
-            id : "random",
-            quantity : orderedProduct.quantity,
-            selectedCombination : orderedProduct.selectedCombination,
-            userId : "random",
-            productId : orderedProduct.id,
-            product : {
-                id : orderedProduct.id,
-                name : orderedProduct.product.name,
-                image : orderedProduct.product.image,
-                storeName : orderedProduct.product.storeName,
-                storeId : orderedProduct.product.storeId,
-                price : orderedProduct.priceAtOrderTime,
-                discount : null
-            }
-        }
+export const convertToCartItems = (
+  orderedProducts: OrderedProductType[],
+): CartItemType[] => {
+  const convertedCartItems = orderedProducts.map((orderedProduct) => {
+    const convertedCartItem = {
+      id: "random",
+      quantity: orderedProduct.quantity,
+      selectedCombination: orderedProduct.selectedCombination,
+      userId: "random",
+      productId: orderedProduct.id,
+      product: {
+        id: orderedProduct.id,
+        name: orderedProduct.product.name,
+        image: orderedProduct.product.image,
+        storeName: orderedProduct.product.storeName,
+        storeId: orderedProduct.product.storeId,
+        price: orderedProduct.priceAtOrderTime,
+        category: orderedProduct.product.category,
+        superTokensUserId: orderedProduct.superTokensUserId,
+        discount: null,
+      },
+    };
 
-        return convertedCartItem
-    });
+    return convertedCartItem;
+  });
 
-    return convertedCartItems
-}
+  return convertedCartItems;
+};
