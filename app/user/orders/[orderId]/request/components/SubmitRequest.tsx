85ee5a2ba60329096be0035f47cde18e90bf41d5
yousuf123456 <muhammadyousuf554456@gmail.com>
2023-09-27 16:38:21 +0500

    Minor Changes

--- app/user/orders/[orderId]/request/components/SubmitRequest.tsx
+++ app/user/orders/[orderId]/request/components/SubmitRequest.tsx
@@ -1,67 +1,79 @@
-"use client"
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
 
-import { LoadingButton } from '@/app/components/LoadingButton'
-import { useAppSelector } from '@/app/store/store'
-import { PackageType } from '@/app/types'
-import { getUpdatedPackages } from '@/app/utils/getUpdatedPackages'
-import { useRouter } from 'next/navigation'
+import { LoadingButton } from "@/app/components/LoadingButton";
+import { useAppSelector } from "@/app/store/store";
+import { PackageType } from "@/app/types";
+import { getUpdatedPackages } from "@/app/utils/getUpdatedPackages";
+import { useRouter } from "next/navigation";
 
-import axios from 'axios'
-import productMinorInfoSlice from '@/app/store/features/productMinorInfoSlice'
+import axios from "axios";
+
 interface SubmitRequestProps {
-  packages : PackageType[];
-  orderId : string;
-  type : "Cancellation" | "Return";
+  packages: PackageType[];
+  orderId: string;
+  type: "Cancellation" | "Return";
 }
 
 export const SubmitRequest: React.FC<SubmitRequestProps> = ({
   packages,
   orderId,
-  type
+  type,
 }) => {
-
   const [isLoading, setIsLoading] = useState(false);
 
-  const selectedOrderedProducts = useAppSelector(state=> state.orderRequests.selectedOrderedProducts);
-  const isAgreedToPolicies = useAppSelector(state=> state.orderRequests.isAgreedToPolicies);
-  const proofImages = useAppSelector(state=> state.orderRequests.proofImages);
-  const feedback = useAppSelector(state=> state.orderRequests.feedback);
+  const selectedOrderedProducts = useAppSelector(
+    (state) => state.orderRequests.selectedOrderedProducts,
+  );
+  const isAgreedToPolicies = useAppSelector(
+    (state) => state.orderRequests.isAgreedToPolicies,
+  );
+  const proofImages = useAppSelector(
+    (state) => state.orderRequests.proofImages,
+  );
+  const feedback = useAppSelector((state) => state.orderRequests.feedback);
 
-  const reasonNotGiven = selectedOrderedProducts.filter((orderedProduct)=> orderedProduct.reason.length === 0);
+  const reasonNotGiven = selectedOrderedProducts.filter(
+    (orderedProduct) => orderedProduct.reason.length === 0,
+  );
 
   const router = useRouter();
 
-  const onClick = ()=> {
-    const {
-      updatedOrderedProducts,
-      updatedPackagesWithUpdatedStatus
+  const onClick = () => {
+    const { updatedOrderedProducts, updatedPackagesWithUpdatedStatus } =
+      getUpdatedPackages(packages, selectedOrderedProducts, type);
 
-    } = getUpdatedPackages(packages, selectedOrderedProducts, type);
+    console.log(updatedOrderedProducts[0].storeId);
+    // setIsLoading(true);
+    // axios.post("../../../../../api/orderRequest", {
+    //   updatedPackages : updatedPackagesWithUpdatedStatus,
+    //   updatedOrderedProducts : updatedOrderedProducts,
+    //   orderFeedback : feedback,
+    //   proofImages : proofImages,
+    //   orderId : orderId,
+    //   type : type
+    // })
+    // .then((res)=> {
+    //   router.push(`/user/orders/${orderId}/request-result?type=${type}&requestId=${res.data.requestId}&process=${res.data.process}`)
+    // })
+    // .catch((e)=> console.log(e))
+    // .finally(()=> setIsLoading(false));
+  };
 
-    setIsLoading(true);
-    axios.post("../../../../../api/orderRequest", {
-      updatedPackages : updatedPackagesWithUpdatedStatus,
-      updatedOrderedProducts : updatedOrderedProducts,
-      orderFeedback : feedback,
-      proofImages : proofImages,
-      orderId : orderId,
-      type : type
-    })
-    .then((res)=> {
-      router.push(`/user/orders/${orderId}/request-result?type=${type}&requestId=${res.data.requestId}&process=${res.data.process}`)
-    })
-    .catch((e)=> console.log(e))
-    .finally(()=> setIsLoading(false));
-  }
-
-  const disabled = !isAgreedToPolicies || !selectedOrderedProducts.length || reasonNotGiven.length > 0
+  const disabled =
+    !isAgreedToPolicies ||
+    !selectedOrderedProducts.length ||
+    reasonNotGiven.length > 0;
 
   return (
-    <div className='w-full flex justify-end'>
-      <LoadingButton onClick={onClick} disabled={isLoading || disabled} isLoading={isLoading}>
+    <div className="flex w-full justify-end">
+      <LoadingButton
+        onClick={onClick}
+        disabled={isLoading || disabled}
+        isLoading={isLoading}
+      >
         Submit Request
       </LoadingButton>
     </div>
-  )
-}
+  );
+};
